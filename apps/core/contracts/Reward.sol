3d8613d92fdce3e97244bc89e930101811a5c891
Tolu Olugbesan <tolugbesan@gmail.com>
2024-11-11 11:42:32 +0000

    Add Reward contract and tests

---
+++ apps/core/contracts/Reward.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+// interface IERC20 {
+//     function transfer(address recipient, uint256 amount) external returns (bool);
+// }
+
+contract Reward {
+    IERC20 public token;
+
+    event Withdrawal(address indexed user, uint256 amount);
+
+    constructor(address _tokenAddress) {
+        token = IERC20(_tokenAddress);
+    }
+
+    function withdraw(uint256 amount) payable public {
+        require(amount > 0, "Withdrawal amount must be greater than zero.");
+        require(token.balanceOf(address(this)) >= amount, "Insufficient contract balance for withdrawal.");
+        token.transfer(msg.sender, amount);
+
+        emit Withdrawal(msg.sender, amount);
+    }
+}
