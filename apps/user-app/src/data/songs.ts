6e904005d629cd6927321e890ec0578d65a2846e
Tolu Olugbesan <tolugbesan@gmail.com>
2024-11-12 07:21:40 +0000

    Update song selection

--- apps/user-app/src/data/songs.ts
+++ apps/user-app/src/data/songs.ts
@@ -1,23 +1,90 @@
 export type Song = {
+  id: number;
   title: string;
   artist: string;
   albumArt: string;
   url: string;
+  apple: string;
+  spotify: string;
+  youtube: string;
 };
 
 export const songs: Song[] = [
   {
-    title: "Song One",
-    artist: "Artist A",
+    id: 1,
+    title: "Starfield",
+    artist: "Bunmi.",
     albumArt:
-      "https://ia801604.us.archive.org/28/items/mbid-76df3287-6cda-33eb-8e9a-044b5e15ffdd/mbid-76df3287-6cda-33eb-8e9a-044b5e15ffdd-829521842.jpg",
-    url: "https://files.freemusicarchive.org/storage-freemusicarchive-org/tracks/1cAC6GdLhQxx2qaUOHGs9uGp2Fhx28UBk3f4G5dJ.mp3",
+      "https://i.scdn.co/image/ab67616d0000b27371d2fc7860fb53a382a7bb88",
+    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731357200/Euterpe/jo8rm5hqmlc4erp1vokv.mp3",
+    apple: "https://music.apple.com/ng/artist/bunmi/1690945658",
+    spotify: "https://open.spotify.com/artist/1qktTPa4kJCpNl2hIr8mTP",
+    youtube: "https://www.youtube.com/channel/UCC6K0qHba5YN15-0OQAR_5Q",
   },
   {
-    title: "Song Two",
-    artist: "Artist B",
+    id: 2,
+    title: "Abeg",
+    artist: "SOLIS4EVR",
     albumArt:
-      "https://cdn.pixabay.com/audio/2024/10/17/15-53-19-182_200x200.jpg",
-    url: "https://cdn.pixabay.com/audio/2024/10/17/audio_5743a1bac5.mp3",
+      "https://i.scdn.co/image/ab67616d0000b273514e92e8ef2361ad12f08a0e",
+    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731360743/Euterpe/gbtq0dqjlwul4vpmjejg.mp3",
+    apple: "https://music.apple.com/ng/artist/solis4evr/1739436116",
+    spotify: "https://open.spotify.com/artist/3ZbW5RPoVdTdsR7JmRtoms",
+    youtube: "https://www.youtube.com/channel/UC9ok1F-7f3Wl1TaSf3G2AUg",
+  },
+  {
+    id: 3,
+    title: "love is like Lagos traffic",
+    artist: "Tega Ethan",
+    albumArt:
+      "https://i.scdn.co/image/ab67616d0000b273aa621853807a728c8e23fd20",
+    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731362621/Euterpe/o0qjyd4e6drddkmertan.mp3",
+    apple: "https://music.apple.com/ng/artist/tega-ethan/1529809681",
+    spotify: "https://open.spotify.com/artist/2B55Oxm3cmN2SsD17Oikz1",
+    youtube: "https://www.youtube.com/channel/UCdQOX6l9kjF1q25EXi0kw8g",
+  },
+  {
+    id: 4,
+    title: "New to Me",
+    artist: "Toriah",
+    albumArt:
+      "https://i.scdn.co/image/ab67616d0000b2732f32bbc80b869a2e58556be3",
+    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731363451/Euterpe/lmrp052dntjle7x6oilp.mp3",
+    apple: "https://music.apple.com/ng/artist/toriah/1742664885",
+    spotify: "https://open.spotify.com/artist/1BtsNTvXfcdez7G645tESW",
+    youtube: "https://www.youtube.com/channel/UCmnUB750WMn4i1PYfDRX-yA",
+  },
+  {
+    id: 5,
+    title: "Sugarcane",
+    artist: "Xpacegirl",
+    albumArt:
+      "https://i.scdn.co/image/ab67616d0000b273676e212f1dd862ca14d40a0e",
+    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731364760/Euterpe/jco75vmhncm263s4m3qr.mp3",
+    apple: "https://music.apple.com/ng/artist/xpacegirl/1637759963",
+    spotify: "https://open.spotify.com/artist/1Gvq4XMDBLFHSZd1yu1UnN",
+    youtube: "https://www.youtube.com/channel/UCUapV_99kcS04jAG1qlXKmA",
+  },
+  {
+    id: 6,
+    title: "Paradise",
+    artist: "SuperJazzClub",
+    albumArt:
+      "https://i.scdn.co/image/ab67616d0000b2738fb7d77d029169c9e47ba644",
+    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731365437/Euterpe/u7ef0hyykknfxkrmfcyg.mp3",
+    apple: "https://music.apple.com/ng/artist/superjazzclub/1467672052",
+    spotify: "https://open.spotify.com/artist/5CINjDZoikcuTmtw3wgPfp",
+    youtube: "https://www.youtube.com/channel/UCwVE8-7xH5EdymHi7UxVAIA",
+  },
+  {
+    id: 7,
+    title: "I Think We Danced (But I Can't Be Sure)",
+    artist: "itsjustrand",
+    albumArt:
+      "https://i.scdn.co/image/ab67616d0000b273961da7b10704a2b6efe45593",
+    url: "https://res.cloudinary.com/din8cizsb/video/upload/v1731366787/Euterpe/elqnfrruyba2maqxzgke.mp3",
+    apple: "https://music.apple.com/ng/artist/rand/1503095243",
+    spotify: "https://open.spotify.com/artist/4MfmEl26HVlsJDUDLsjTXp",
+    youtube: "https://www.youtube.com/channel/UC5T1_1lzm1M-uWbnJlyUJkg",
   },
 ];
--- apps/user-app/src/partials/feed/StreamingLinks.tsx
+++ apps/user-app/src/partials/feed/StreamingLinks.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import { type Song } from "@/data/songs";
 
 function StreamingLinks({ song }: { song: Song }) {
+  const platforms = [
+    { name: "Apple Music", url: song.apple },
+    { name: "Spotify", url: song.spotify },
+    { name: "YouTube Music", url: song.youtube },
+  ];
   return (
     <div className="flex gap-3">
-      {["Apple Music", "Spotify", "YouTube Music"].map((link, index) => (
+      {platforms.map((platform) => (
         <Button
-          key={index}
+          key={platform.name}
           className="rounded-[120px] bg-white px-4 py-2 font-figtree text-xs font-semibold tracking-[-0.02em] text-[#0F0F0F] hover:bg-white/90"
+          asChild
         >
-          {link}
+          <Link href={platform.url} target="_blank" rel="noopener noreferrer">
+            {platform.name}
+          </Link>
         </Button>
       ))}
     </div>
