c4c2711556e32906dfdb6beae3f71c6f02696b09
Tolu Olugbesan <tolugbesan@gmail.com>
2024-11-12 08:35:52 +0000

    Fix Header

--- apps/user-app/src/partials/Header.tsx
+++ apps/user-app/src/partials/Header.tsx
@@ -12,13 +12,13 @@ function Header() {
   return (
     <header className="sticky top-0 z-50 flex h-[3.25rem] items-center justify-between border-b-[0.2px] border-[#303033]/80 bg-white/[0.02] px-6 py-3">
       <div className="flex">
-        <div className="hidden items-center gap-2 lg:flex">
+        <div className="flex items-center gap-2 lg:flex">
           <Link href="/">
             <h2 className="text-sm font-bold">Euterpe</h2>
           </Link>
           <Badge
             variant="outline"
-            className="text-nowrap font-azeret text-[0.563rem]"
+            className="hidden text-nowrap font-azeret text-[0.563rem] md:inline-flex"
           >
             Listen to Earn
           </Badge>
